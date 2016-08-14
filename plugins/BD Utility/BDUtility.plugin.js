a11d2c9ce5ba29c8d77d681e416b1b0bb9f064e6
Natsulus <natsulus@hotmail.com>
2016-08-15 01:39:29 +1000

    BDUtility v1.1.0

--- plugins/BD Utility/BDUtility.plugin.js
+++ plugins/BD Utility/BDUtility.plugin.js
@@ -1,8 +1,51 @@
 //META{"name":"BDUtility"}*//
 
-class BDUtility {
+class BDEvent {
+    constructor(name) {
+        this.name = name;
+        this.callbacks = [];
+    }
+
+    registerCallback(callback) {
+        this.callbacks.push(callback);
+    }
+}
+
+class BDEventSystem {
+    constructor() {
+        this.events = {};
+    }
+
+    registerEvent(eventName) {
+        this.events[eventName] = new BDEvent(eventName);
+    }
+
+    trigger(eventName, ...eventArgs) {
+        if (this.events[eventName]) {
+            this.events[eventName].callbacks.forEach(callback => {
+                callback(...eventArgs);
+            });
+        }
+    }
+
+    on(eventName, callback) {
+        if (!this.events[eventName]) this.registerEvent(eventName);
+        this.events[eventName].registerCallback(callback);
+    }
+}
+
+class BDUtility extends BDEventSystem {
     constructor(options) {
-        this.loadScript("lz-string", "https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js");
+        super();
+        if (!window.LZString) {
+            const waitInterval = setInterval(() => {
+                if (window.LZString) clearInterval(waitInterval);
+                if (window.jQuery) {
+                    this.loadScript("lz-string", "https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js");
+                    clearInterval(waitInterval);
+                }
+            }, 200);
+        }
 
         this.repository = options && options.repository;
         this.plugin = options && options.plugin;
@@ -28,9 +71,9 @@ class BDUtility {
 
     loadScript(item, url) {
         const script = $("<script>", {
-            type: "text/javascript",
+            id: item,
             src: url,
-            id: item
+            type: "text/javascript"
         });
 
         $("head").append(script);
@@ -49,15 +92,15 @@ class BDUtility {
     getJSON(url) {
         return new Promise((resolve, reject) => {
             $.ajax({
-                type: "GET",
                 dataType: "json",
-                url,
                 error(...err) {
                     reject(err);
                 },
                 success(data) {
                     resolve(data);
-                }
+                },
+                type: "GET",
+                url
             });
         });
     }
@@ -66,14 +109,14 @@ class BDUtility {
         return new Promise((resolve, reject) => {
             if (!this.repository) reject("No Repository Defined");
             $.ajax({
-                type: "GET",
-                url: `https://raw.githubusercontent.com/${this.repository}/${branch}/${file}.json`,
                 error(...err) {
                     reject(err);
                 },
                 success(data) {
                     resolve(JSON.parse(data));
-                }
+                },
+                type: "GET",
+                url: `https://raw.githubusercontent.com/${this.repository}/${branch}/${file}.json`
             });
         });
     }
@@ -82,8 +125,6 @@ class BDUtility {
         return new Promise((resolve, reject) => {
             if (!this.repository) reject("No Repository Defined");
             $.ajax({
-                type: "GET",
-                url: `https://api.github.com/repos/${this.repository}/commits/${branch}`,
                 beforeSend: xhr => {
                     xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
                 },
@@ -92,7 +133,9 @@ class BDUtility {
                 },
                 success(data) {
                     resolve(data.sha);
-                }
+                },
+                type: "GET",
+                url: `https://api.github.com/repos/${this.repository}/commits/${branch}`
             });
         });
     }
@@ -102,14 +145,14 @@ class BDUtility {
             if (!this.repository) reject("No Author or Repository Defined");
             this.getGithubHash(options.branch).then(hash => {
                 $.ajax({
-                    type: "GET",
-                    url: `https://rawcdn.githack.com/${this.repository}/${hash}/${options.file}`,
                     error(...err) {
                         reject(err);
                     },
                     success(data) {
                         resolve(data);
-                    }
+                    },
+                    type: "GET",
+                    url: `https://rawcdn.githack.com/${this.repository}/${hash}/${options.file}`
                 });
             }).catch(err => {
                 reject(err);
@@ -118,11 +161,11 @@ class BDUtility {
     }
 
     saveStorage(store, data) {
-        localStorage.setItem(store, LZString.compress(JSON.stringify(data)));
+        localStorage.setItem(store, window.LZString.compress(JSON.stringify(data)));
     }
 
     loadStorage(store, def) { // only to be used on storage saved with saveStorage
-        return localStorage.getItem(store) && JSON.parse(LZString.decompress(localStorage.getItem(store))) || def || {};
+        return localStorage.getItem(store) && JSON.parse(window.LZString.decompress(localStorage.getItem(store))) || def || {};
     }
 
     getReactInstance(node) {
@@ -138,9 +181,172 @@ class BDUtility {
     }
 
     getReactProps(node) {
-        return (obj => {
-            if (obj) return obj.props;
-        })(this.getReactObject(node));
+        return (inst => {
+            if (inst) return inst._currentElement._owner._instance.props;
+        })(this.getReactInstance(node));
+    }
+
+    eventObserver(ev) {
+        // Voice Join/Leave, Leaving doesn't provide user unfortunately
+        if (ev.addedNodes.length === 1 && ev.removedNodes.length === 1 && ev.target.className.includes("channel-voice")) {
+            if (ev.addedNodes[0].className && ev.addedNodes[0].className.includes("channel-voice-states")) {
+                const props = this.getReactProps(ev.addedNodes[0].childNodes[0]);
+
+                return this.trigger("joinVoiceChannel", ev, props.user, props.channel);
+            } else if (ev.removedNodes[0].className && ev.removedNodes[0].className.includes("channel-voice-states")) {
+                const props = this.getReactProps(ev.target);
+
+                return this.trigger("leaveVoiceChannel", ev, props.channel);
+            }
+        }
+
+        // Tooltips, can't get React Data of Owner unfortunately
+        if (ev.target.className === "tooltips") {
+            if (ev.addedNodes.length === 1) return this.trigger("tooltipOver", ev);
+
+            return this.trigger("tooltipOff", ev);
+        }
+
+        // Switching Channel/Server, switchServerChannel for when switching server because can't get old channel
+        if (ev.addedNodes.length === 1 && ev.removedNodes.length === 1 && ev.target.nodeName === "SPAN") {
+            if (ev.target.className === "channel-name") {
+                const oldProps = $(`a > .channel-name:contains(${ev.removedNodes[0].data})`).getReactProps()[0];
+                const newProps = $(`a > .channel-name:contains(${ev.addedNodes[0].data})`).getReactProps()[0];
+
+                if (oldProps) this.trigger("switchTextChannel", ev, oldProps.channel, newProps.channel);
+                else this.trigger("switchServerChannel", ev, newProps.channel);
+
+                return this.trigger("loadTextChannel", ev, newProps.channel);
+            }
+            const props = this.getReactProps(ev.target);
+
+            if (Reflect.has(props, "guild")) {
+                this.trigger("switchServer", ev, props.guild);
+                this.trigger("loadServer", ev, props.guild);
+
+                return;
+            }
+        }
+
+        // Load Friend List Page
+        if (ev.addedNodes.length === 1 && ev.removedNodes.length === 1 && ev.target.nodeName === "SECTION" && ev.target.className === "flex-horizontal flex-spacer") {
+            if (ev.addedNodes[0].id === "friends") {
+                $(ev.addedNodes[0]).find("div.scroller.friends-table-body > span").children().each((i, el) => {
+                    const props = this.getReactProps(el.childNodes[0]);
+
+                    if (Reflect.has(props, "user")) {
+                        this.trigger("loadFriend", el, props.user, props.mutualGuilds);
+                    }
+                });
+
+                return this.trigger("loadFriendList", ev);
+            }
+        }
+
+        // User Typing Notification
+        if (ev.addedNodes.length === 1 && ev.removedNodes.length === 1 && ev.target.nodeName === "FORM" && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("typing")) {
+            const props = this.getReactProps(ev.target);
+
+            if (Reflect.has(props, "channel")) {
+                return this.trigger("userTyping", ev, props.channel);
+            }
+        }
+
+        // Show Member List
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className === "channel-members-wrap" && ev.target.className === "content flex-spacer flex-horizontal") {
+            return this.trigger("showMemberList", ev);
+        }
+
+        // DM Page to Server
+        if (ev.addedNodes.length === 1 && ev.target.className === "flex-vertical channels-wrap") {
+            const props = this.getReactProps(ev.addedNodes[0]);
+
+            if (Reflect.has(props, "guild")) {
+                return this.trigger("loadServer", ev, props.guild);
+            }
+        }
+
+        // Load each new friend row
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("friends-row")) {
+            const props = this.getReactProps(ev.addedNodes[0].childNodes[0]);
+
+            if (Reflect.has(props, "user")) {
+                return this.trigger("loadFriend", ev, props.user, props.mutualGuilds);
+            }
+        }
+
+        // Popouts
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("popout")) {
+            if (ev.addedNodes[0].childNodes[0].className.includes("user-popout")) {
+                const props = this.getReactProps(ev.addedNodes[0].childNodes[0]);
+
+                if (Reflect.has(props, "user")) {
+                    return this.trigger("userPopout", ev, props.user);
+                }
+            } else if (ev.addedNodes[0].childNodes[0].className.includes("undefined")) {
+                const props = this.getReactProps(ev.addedNodes[0].childNodes[0]);
+
+                if (Reflect.has(props, "channel")) {
+                    return this.trigger("pinnedPopout", ev, props.channel);
+                }
+            } else if (ev.addedNodes[0].childNodes[0].className.includes("recent-mentions-popout")) {
+                return this.trigger("mentionsPopout", ev);
+            }
+        }
+
+        // Load User to Member List
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("member") && ev.target.className.includes("scroller channel-members")) {
+            const props = this.getReactProps(ev.addedNodes[0]);
+
+            if (Reflect.has(props, "user")) {
+                return this.trigger("loadMemberListUser", ev, props.user, props.activity);
+            }
+        }
+
+        // Context Menu
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("context-menu")) {
+            if (ev.target.className.includes("item-subMenu")) {
+                const props = this.getReactProps(ev.target);
+
+                if (props) return this.trigger("subContextMenu", ev, props);
+            }
+            const props = this.getReactProps(ev.addedNodes[0]);
+
+            if (props) return this.trigger("contextMenu", ev, props);
+        }
+
+        // Hide Member list
+        if (ev.removedNodes.length === 1 && ev.removedNodes[0].className === "channel-members-wrap" && ev.target.className === "content flex-spacer flex-horizontal") {
+            return this.trigger("hideMemberList", ev);
+        }
+
+        // Message on Switch
+        if (ev.addedNodes.length === 1 && ev.addedNodes[0].className && ev.addedNodes[0].className.includes("messages-wrapper") && ev.target.className.includes("flex-spacer flex-vertical")) {
+            $(ev.addedNodes[0]).find(".message").each((i, el) => {
+                const props = this.getReactProps(el);
+
+                if (Reflect.has(props, "message")) {
+                    this.trigger("message", el, props.message, props.channel);
+                }
+            });
+
+            return;
+        }
+
+        // New Message
+        if (ev.addedNodes.length === 1 && ev.target.className.includes("scroller messages")) {
+            $(ev.addedNodes[0]).find(".message").each((i, el) => {
+                const props = this.getReactProps(el);
+
+                if (Reflect.has(props, "message")) {
+                    this.trigger("message", el, props.message, props.channel);
+                }
+            });
+
+            return;
+        }
+
+        //console.log(ev);
     }
 
     // Meta Data //
@@ -154,11 +360,11 @@ class BDUtility {
     }
 
     getVersion() {
-        return "1.0.0";
+        return "1.1.0";
     }
 
     getAuthor() {
-        return "Natsulus";
+        return `<a href="https://github.com/Natsulus/BD-Repo" target="_BLANK">Natsulus</a>`;
     }
 
     load() {
@@ -183,7 +389,56 @@ class BDUtility {
         subtree: true
     });
 
-    // get BD Utility version
+    const currentVersion = BDUtility.prototype.getVersion();
+    // get latest BD Utility version
     // compare version with BDUtility.prototype.getVersion()
     // display notice if new version
 })(); // use IIFE to avoid polluting global namespace
+
+(function loadReactPlugins($) {
+    if (window.jQuery) {
+        $.fn.getReactInstance = function() {
+            const arr = [];
+
+            this.each((i, el) => {
+                arr.push(el[Object.keys(el).find(key => {
+                    return key.startsWith("__reactInternalInstance");
+                })]);
+            });
+
+            return arr;
+        };
+
+        $.fn.getReactObject = function() {
+            const arr = [];
+
+            this.each((i, el) => {
+                const inst = el[Object.keys(el).find(key => {
+                    return key.startsWith("__reactInternalInstance");
+                })];
+
+                if (inst) arr.push(inst._currentElement._owner._instance);
+                else arr.push(inst);
+            });
+
+            return arr;
+        };
+
+        $.fn.getReactProps = function() {
+            const arr = [];
+
+            this.each((i, el) => {
+                const inst = el[Object.keys(el).find(key => {
+                    return key.startsWith("__reactInternalInstance");
+                })];
+
+                if (inst) arr.push(inst._currentElement._owner._instance.props);
+                else arr.push(inst);
+            });
+
+            return arr;
+        };
+    } else {
+        setTimeout(() => {loadReactPlugins(window.jQuery);}, 50);
+    }
+})(window.jQuery);
--- plugins/BD Utility/README.md
+++ plugins/BD Utility/README.md
@@ -1,1 +1,92 @@
-A required plugin for my other plugins. Contains utility functions used by my other plugins. Just download it to your plugin folder and it works. No need to enable it.
+**Current Version**: 1.1.0
+
+Contains utility functions used by some of my other plugins. Just download it to your plugin folder and it works. No need to enable it.
+
+If you're a plugin developer, feel free to use the utility plugin. You can pull out code to avoid dependency if you wish, but please credit.
+
+## Console
+
+Adds plugin name with choice of style to the front of console logging, helps differentiate which plugin is doing what.
+
+## Utilities
+
+- loadScript: Takes an id and url, appends script of url with id
+- unloadScript: Takes an id, removes element with id
+- getUser: Returns the logged in User
+- getJSON: Takes a URL, returns Promise with data
+- getGithubJSON: Takes branch and filename w/o extension, returns Promise with data
+- getGithubHash: Takes a branch, returns Promise with hash
+- getCDNFile: Takes option object with branch and filename, returns Promise with data
+- saveStorage: Takes store name and data, compress with LZString and save in localStorage
+- loadStorage: Loads from localStorage, uncompress with LZString and returns data
+- getReactInstance: Returns React Data Instance or undefined
+- getReactObject: Returns React Data Object or undefined
+- getReactProps: Returns React Data Props or undefined
+- $.fn.getReact____: Applies above to each element in the jQuery selector and returns an array containing the React Data for each element
+
+## Events
+
+Currently has the following events (provides event/element as first callback argument):
+
+- **joinVoiceChannel**: a user joins a voice channel, provides a user & voice channel
+- **leaveVoiceChannel**:  a user leaves a voice channel, provides a voice channel
+- **tooltipOver**: you hover over something and create a tooltip, provides nothing
+- **tooltipOff**: you leave the hover area and remove a tooltip, provides nothing
+- **switchTextChannel**: you switch between 2 text channels of the same server, provides old text channel and new text channel
+- **switchServerChannel**: you switch servers, causing a switch of text channels, provides a text channel
+- **loadTextChannel**: a text channel loads, provides a text channel
+- **switchServer**: you switch between 2 servers, provides a server
+- **loadServer**: a server loads, provides a server
+- **loadFriendList**: friend list page loads, provides nothing
+- **loadFriend**: friend loads on friend list, provides user and mutual servers
+- **userTyping**: user is typing notification, provides text channel
+- **showMemberList**: member list gets toggled on, provides nothing
+- **hideMemberList**: member list gets toggled off, provides nothing
+- **loadMemberListUser**: user gets loaded into member list, provides user and activity
+- **userPopout**: user popout modal opens, provides a user
+- **pinnedPopout**: pinned messages popout opens, provides a text channel
+- **mentionsPopout**: mentions popout opens, provides nothing
+- **contextMenu**: context menu appears, provides raw react props
+- **subContextMenu**: sub-context menu appears, provides raw react props
+- **message**: message is loaded, provides a message and user
+
+Events may be triggered by something else I didn't prepare for.
+
+If something unexpected happens, please create an issue or contact Natsulus with steps to reproduce whatever bug/undesired result which will help to fix it.
+
+## Example
+
+```js
+start() {
+    this.utility  = new BDUtility({
+        repository: "Natsulus/BD-Repo",
+        plugin: this,
+        console: "My Plugin",
+        style: "font-weight: bold; color: maroon;"
+    });
+    
+    this.utility.info("Logged User:", this.utility.getUser());
+    // [My Plugin] Logged User:     USER-OBJECT
+    
+    this.eventListeners();
+}
+
+observer(ev) {
+    this.utility.eventObserver(ev);
+}
+
+eventListeners() {
+    this.utility.on("message", (ev, message, user) => {
+        this.utility.log(message, user);
+    });
+    this.utility.on("switchTextChannel", (ev, oldChannel, newChannel) => {
+        this.utility.log(oldChannel, newChannel);
+    });
+}
+```
+
+### Planned
+
+- Use own observer or intercept BD observer to avoid conflict of multiple plugins utilising it.
+- More events (please tell Natsulus if you want any events added)
+- Add an updater to keep this utility up-to-date
